e72d5d1d99b44a9c6f3f5c7c0c32d516177103fc
paragspatil <paragspatil123@gmail.com>
2020-08-12 10:24:42 +0530

    Stage 3 in progress commit

--- File Server/task/src/client/Main.java
+++ File Server/task/src/client/Main.java
@@ -4,17 +4,76 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Main {
+
     private static final String ADDRESS = "127.0.0.1";
     private static final int PORT = 23456;
     public static void main(String[] args) {
+        boolean exitFlag = true;
+        Scanner scanner = new Scanner(System.in);
         try(Socket socket = new Socket(ADDRESS,PORT);
             DataInputStream input = new DataInputStream(socket.getInputStream());
             DataOutputStream output = new DataOutputStream(socket.getOutputStream())){
-            output.writeUTF("Give me everything you have!");
-            System.out.println("Sent: Give me everything you have!");
-            String msg = input.readUTF();
-            System.out.println("Received: " + msg);
+            while (exitFlag) {
+                System.out.println("Enter action (1 - get the file, 2 - create a file, 3 - delete the file):");
+                String choice = scanner.nextLine();
+                output.writeUTF(choice);
+                switch (choice) {
+                    case "2":
+                        System.out.println("Enter filename:");
+                        String fileName = scanner.nextLine();
+                        System.out.println("Enter file content:");
+                        String content = scanner.nextLine();
+                        output.writeUTF(fileName);
+                        output.writeUTF(content);
+                        System.out.println("The request was sent.");
+                        String status = input.readUTF();
+                        if(status.equals("200")){
+                            System.out.println("Ok, the response says that the file was created!");
+                        }
+                        else {
+                            System.out.println("Ok, the response says that creating the file was forbidden!");
+                        }
+                        break;
 
+                    case "1":
+                        System.out.println("Enter filename:");
+                        String getFileName = scanner.nextLine();
+                        output.writeUTF(getFileName);
+                        System.out.println("The request was sent.");
+                        String getFileStatus = input.readUTF();
+                        if(getFileStatus.equals("200")){
+                            String fileContent = input.readUTF();
+                            System.out.println("Ok, the content of the file is: " + fileContent);
+                        }
+                        else {
+                            System.out.println("Ok, response says that the file was not found!");
+                        }
+
+
+                        break;
+
+                    case "3":
+                        System.out.println("Enter filename:");
+                        String deleteFileName = scanner.nextLine();
+                        output.writeUTF(deleteFileName);
+                        System.out.println("The request was sent.");
+                        String deleteStatus = input.readUTF();
+                        if(deleteStatus.equals("200")){
+                            System.out.println("Ok, the response says that the file was successfully deleted!");
+                        }
+                        else{
+                            System.out.println("Ok, the response says that the file was not found!");
+                        }
+                        break;
+
+                    case "exit":
+                       // output.writeUTF(choice);
+                        exitFlag = false;
+                        break;
+
+
+                }
+            }
         }
         catch (Exception e){
             e.printStackTrace();
--- File Server/task/src/server/Main.java
+++ File Server/task/src/server/Main.java
@@ -1,4 +1,6 @@
 package server;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -8,25 +10,87 @@ import java.net.*;
 public class Main {
     private static int PORT = 23456;
     public static void main(String[] args) {
-        System.out.println("Server started!");
-        try(ServerSocket server =new ServerSocket(PORT)){
-              try(Socket socket = server.accept();
-                  DataInputStream input = new DataInputStream(socket.getInputStream());
-                   DataOutputStream output = new DataOutputStream(socket.getOutputStream())){
+        boolean exitFlag = true;
+        //System.out.println("Server started!");
+        //System.out.println(System.getProperty("user.dir"));
+        try (ServerSocket server = new ServerSocket(PORT)) {
 
-                  String msg = input.readUTF();
-                  System.out.println("Received: " + msg);
-                  output.writeUTF("All files were sent!");
-                  System.out.println("Sent: All files were sent!");
+                try (Socket socket = server.accept();
+                     DataInputStream input = new DataInputStream(socket.getInputStream());
+                     DataOutputStream output = new DataOutputStream(socket.getOutputStream())) {
+                    while (exitFlag) {
+                        String msg = input.readUTF();
+                        //System.out.println(msg);
+                        switch (msg) {
+                            case "2":
+                                //System.out.println("enterring case 1");
+                                String fileName = input.readUTF();
+                                String fileContent = input.readUTF();
+                                String filePath = "E:\\trash\\File Server\\File Server\\task\\src\\server\\data\\" + fileName;
+                                File newFile = new File(filePath);
+                                //System.out.println(newFile.exists());
+                                if (newFile.exists()) {
+                                    output.writeUTF("403");
+                                } else {
 
-              }
-              catch (Exception e){
-                  System.out.println("something went wrong with server");
-              }
+
+                                    try (PrintWriter writer = new PrintWriter(newFile)) {
+                                        writer.println(fileContent);
+                                        output.writeUTF("200");
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+
+                                break;
+
+                            case "1":
+                                String getFileName = input.readUTF();
+                                String getFilePath = "E:\\trash\\File Server\\File Server\\task\\src\\server\\data\\" + getFileName;
+                                File getFile = new File(getFilePath);
+                                if(getFile.exists()){
+                                    String content  = new String(Files.readAllBytes(Paths.get(getFilePath)));
+                                    output.writeUTF("200");
+                                    output.writeUTF(content);
+                                }
+                                else {
+                                    output.writeUTF("404");
+                                }
+                                break;
+
+                            case "3":
+                                String deleteFileName = input.readUTF();
+                                String deleteFilePath = "E:\\trash\\File Server\\File Server\\task\\src\\server\\data\\" + deleteFileName;
+                                File deleteFile = new File(deleteFilePath);
+                                if(deleteFile.exists()){
+                                    deleteFile.delete();
+                                    output.writeUTF("200");
+                                }
+                                else {
+                                    output.writeUTF("404");
+                                }
+                                break;
+
+                            case "exit":
+                                exitFlag = false;
+                                break;
+                        }
+                    }
+
+                } catch (Exception e) {
+                    System.out.println("something went wrong with server");
+                }
+            }
+
+        catch(Exception e){
+                System.out.println("something went wrong with server");
+            }
+
         }
-        catch (Exception e){
-            System.out.println("something went wrong with server");
-        }
+
+
+        public static void createFile(String fileName, String fileContent){
+          //.../server/data/
 
         }
 }
